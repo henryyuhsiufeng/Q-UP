d213c78b7c8cd0a7f7b42ef85c2f96e6b04e8979
Henry Feng <henryfeng12345@gmail.com>
2019-10-06 13:57:47 -0500

    create basic landing & and directory structure

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Q-UP' });
+  res.render('landing', { title: 'Q-UP' });
 });
 
 module.exports = router;
